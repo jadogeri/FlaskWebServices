2073a8d1aeb7da71a19171a81e361ee8a19d230c
jadogeri <jadogeri@uno.edu>
2024-08-03 18:48:34 -0500

    added Temperature.py and Cooking.py

---
+++ models/Cooking.py
@@ -0,0 +1,16 @@
+# static double teaspoonsToTablespoons(double teaspoons) Returns number of TBSPs from TSP, 1 tablespoon = 3 teaspoons
+
+# static double tablespoonsToTeaspoons(double tablespoons) Returns number of TSPs from TBSP, 1 tablespoon = 3 teaspoons
+
+# static double tablespoonsToCups(double tablespoons) Returns number of CUPs from TBSP, 1 cup = 16 tablespoons
+
+# static double cupsToTablespoons(double cups) Returns number of TBSPs from CUPs, 1 cup = 16 tablespoons
+
+# static double ouncesToCups(double ounces) Returns number of CUPs from OUNCEs, 1 cup = 8 ounces
+
+# static double cupsToOunces(double cups) Returns number of OUNCEs from CUPs, 1 cup = 8 ounces
+
+# static double cupsToPints(double cups) Returns number of PINTs from CUPs, 1 pint = 2 cups
+
+# static double pintsToCups(double pints) Returns number of CUPs from PINTs, 1 pint = 2 cups
+
---
+++ models/Temperature.py
@@ -0,0 +1,11 @@
+# static double celsiusToFahrenheit(double celsius) Returns degrees Fahrenheit from degrees Celsius, F= 9 C +32 5
+
+# static double celsiusToKelvin(double celsius) Returns degrees Kelvin from degrees Celsius, K =C +273.15
+
+# static double fahrenheitToCelsius(double fahrenheit) Returns degrees Celsius from degrees Fahrenheit, C= 5 ( F−32) 9
+
+# static double fahrenheitToKelvin(double fahrenheit) Returns degrees Kelvin from degrees Fahrenheit, K= 5 ( F +459.67) 9
+
+# static double kelvinToFahrenheit(double kelvin) Returns degrees Fahrenheit from degrees Kelvin, F= 9 K −459.67 5
+
+# static double kelvinToCelsius(double kelvin) Returns degrees Celsius from degrees Kelvin, C=K−273.15
